0ce9dbec605398532ced6a074ca57ad258216bac
Mustafa <mzm25191@gmail.com>
2026-08-26 09:57:51 +0300

    fix: cast VAPID key buffer type for strict TS compatibility

--- components/web-push-registration.tsx
+++ components/web-push-registration.tsx
@@ -34,7 +34,7 @@ async function registerAndSubscribe() {
 
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+      applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as BufferSource,
     });
 
     await saveSubscription(subscription);
